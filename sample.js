56f44d610e2319735897a2c2b525c0e73da1135b
kyonekura <yonedayo1002@gmail.com>
2020-03-11 04:23:27 +0900

    second commit

--- sample.js
+++ sample.js
@@ -1,53 +1,10 @@
-var
-cursor = $(".cursor"),
-follower = $(".follower"),
-cWidth = 8, //カーソルの大きさ
-fWidth = 40, //フォロワーの大きさ
-delay = 10, //数字を大きくするとフォロワーがより遅れて来る
-mouseX = 0, //マウスのX座標
-mouseY = 0, //マウスのY座標
-posX = 0, //フォロワーのX座標
-posY = 0; //フォロワーのX座標
-
-//カーソルの遅延アニメーション
-//ほんの少ーーーしだけ遅延させる 0.001秒
-TweenMax.to({}, .001, {
-  repeat: -1,
-  onRepeat: function() {
-    posX += (mouseX - posX) / delay;
-    posY += (mouseY - posY) / delay;
-    
-    TweenMax.set(follower, {
-        css: {    
-          left: posX - (fWidth / 2),
-          top: posY - (fWidth / 2)
-        }
-    });
-    
-    TweenMax.set(cursor, {
-        css: {    
-          left: mouseX - (cWidth / 2),
-          top: mouseY - (cWidth / 2)
-        }
-    });
-  }
+$(function() {
+  let style;
+  $(window).mousemove(function(e) {
+    $("#txt1").val(e.clientX);
+    $("#txt2").val(e.clientY);
+    style = "ellipse(150px 150px at " + e.clientX + "px " + e.clientY + "px)";
+    console.log(style);
+    $(".circle").css("clip-path", style);
+  });
 });
-
-//マウス座標を取得
-$(document).on("mousemove", function(e) {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-});
-
-$("a").on({
-  "mouseenter": function() {
-    cursor.addClass("is-active");
-    follower.addClass("is-active");
-  },
-  "mouseleave": function() {
-    cursor.removeClass("is-active");
-    follower.removeClass("is-active");
-  }
-});
-
-
